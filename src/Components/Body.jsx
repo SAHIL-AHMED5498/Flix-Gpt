586edbfa674021de8388c1ce28fe7dc469066aac
Sahil Ahmed <ahmedsahil5498@gmail.com>
2025-08-03 11:21:09 +0530

    omdb api

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
+
+
 const Body=()=>{
+    const [datas,setDatas]=useState({data:"loading"});
+    let Json=""
+    const search=useRef();
+
+    const handleClick=()=>{
+        fetchData(search.current.value)
+
+    }
+  
+    const fetchData=async(s)=>{
+    const data=await fetch(`http://www.omdbapi.com/?t=${s}&y=2020&plot=full&apikey=dc67d842`);
+     Json=await data.json();
+     setDatas(Json);
+
+     
+    }
+ 
 
     
 
 
-    return(<div>
-    <h1 className="bg-slate-500 relative top-14">Body</h1>
+    return(<div className="bg-slate-100 relative top-14">
+
+    <h1 >Body  componeny</h1>
+    
+    <input type="text" placeholder="search" ref={search} className="bg-slate-200 p-2 m-2 rounded border "/>
+    
+    <button onClick={handleClick} className="text-black bg-blue-200 p-2 m-2 active:scale-95 hover:bg-blue-500">Search</button>
+    {datas && <p>fetched data :{JSON.stringify(datas, null, 2)}</p>}
     </div>)
 }
 
---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDQwODZkZWZkZjA0YjliNGMyZWUzNjkyZGYzZTliMyIsIm5iZiI6MTc1NDE2NjMwMS44MzUsInN1YiI6IjY4OGU3NDFkNTE2ZmMyMmIwZWE0NjMyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lt61uV9bA2pfT3mXanD2adY_JFzDGNEaBMgN1V_vL0o'
+  }
+};
